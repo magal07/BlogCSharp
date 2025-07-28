61476465f380bc961d0ed61ec1efcaf99b241cd7
Marcelo Magalhães <celoxd01@gmail.com>
2025-07-28 03:20:46 -0300

    Add ConnectionString and Drap Services

---
+++ BlogCSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using BlogCSharp.Models;
+using Dapper.Contrib.Extensions;
+
+using Microsoft.Data.SqlClient;
+
+namespace BlogCSharp {
+    
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost;Database=Blog;User=sa;Password=1q2w3e4r@#$;Encrypt=False";
+        static void Main(string[] args)
+        {
+            ReadUsers();
+        }
+
+        public static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+        }
+    }
+}
