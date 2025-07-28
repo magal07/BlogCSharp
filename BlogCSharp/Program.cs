32ee7f1278c31d72945883c273e5d0e2c7a6dcfc
Marcelo Magalhães <celoxd01@gmail.com>
2025-07-28 12:20:33 -0300

    Create folder Repositories and Entities User and Role. Add UNIQUE CONNECTION_STRING

--- BlogCSharp/Program.cs
+++ BlogCSharp/Program.cs
@@ -1,28 +1,44 @@
 ﻿using BlogCSharp.Models;
+using BlogCSharp.Repositories;
 using Dapper.Contrib.Extensions;
 
 using Microsoft.Data.SqlClient;
 
 namespace BlogCSharp {
     
-    class Program
+    static class Program
     {
         private const string CONNECTION_STRING = @"Server=localhost;Database=Blog;User=sa;Password=1q2w3e4r@#$;Encrypt=False";
         static void Main(string[] args)
         {
-            ReadUsers();
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+            ReadUsers(connection);
+            ReadRoles(connection);
+            
+            // ReadUser();
+            // CreateUser();
+            // UpdateUser();
+            // DeleteUser();
+
+            connection.Close();
         }
 
-        public static void ReadUsers()
+        public static void ReadUsers(SqlConnection connection)
         {
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                var users = connection.GetAll<User>();
-                foreach (var user in users)
-                {
-                    Console.WriteLine(user.Name);
-                }
-            }
-        }
+            var repository = new UserRepository(connection);
+            var users = repository.Get();
+
+            foreach (var user in users)
+                Console.WriteLine(user.Name);
+        } 
+        public static void ReadRoles(SqlConnection connection)
+        {
+            var repository = new RoleRepository(connection);
+            var roles = repository.Get();
+
+            foreach (var role in roles)
+                Console.WriteLine(role.Name);
+        }   
     }
 }
